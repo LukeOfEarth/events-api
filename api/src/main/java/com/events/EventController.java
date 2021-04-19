3d282c8415c8b52846165b89e36f41b097f5811e
Luke van Aardt <vanaardtluke@gmail.com>
2021-04-19 07:39:14 +0200

    Added error handling on GET By Id Route

--- api/src/main/java/com/events/EventController.java
+++ api/src/main/java/com/events/EventController.java
@@ -1,9 +1,12 @@
 package com.events;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class EventController {
@@ -16,7 +19,14 @@ public class EventController {
     }
 
     @GetMapping("events/{id}")
-    public Event getEventById(@PathVariable Integer id) { return service.get(id); }
+    public ResponseEntity<Event> getEventById(@PathVariable Integer id) {
+        try {
+            Event event = service.get(id);
+            return new ResponseEntity<Event>(event, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<Event>(HttpStatus.NOT_FOUND);
+        }
+    }
 
     @PostMapping("newEvent")
     public void createEvent(@RequestBody Event event) { service.save(event); }
