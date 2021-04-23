a43a58c5b0189ed408f4d2fe25317659aa8e15a8
Matthew <pilky.matt@gmail.com>
2021-04-23 11:13:48 +0200

    Added self and all links to models using HATEAOS. Will update with action links once requests are finished

--- api/src/main/java/com/events/users/UserController.java
+++ api/src/main/java/com/events/users/UserController.java
@@ -1,46 +1,51 @@
 package com.events.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-
-import java.util.List;
+import org.springframework.hateoas.Link;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import java.util.NoSuchElementException;
+
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
     @Autowired
-    private UserService service;
+    private final UserService service;
+    private final UserResourceAssembler assembler;
+
+    public UserController(UserService service, UserResourceAssembler assembler) {
+        this.service = service;
+        this.assembler = assembler;
+    }
 
     @GetMapping("users")
     public CollectionModel<EntityModel<User>> list() {
+        //Create a collection model of users
+        CollectionModel<EntityModel<User>> userModels = assembler.toCollectionModel(service.listAll());
 
-        List<EntityModel<User>> users = service.listAll().stream()
-                .map(user -> getUserById(id))
-                .collect(Collectors.toList());
+        //Create a link to this function and add to the collection model
+        Link selfLink = linkTo(methodOn(UserController.class).list()).withSelfRel();
+        userModels.add(selfLink);
 
-        return new CollectionModel<>(users,
-                linkTo(methodOn(UserController.class).list()).withSelfRel());
+        //Return a collection model containing all users, and a link to this function
+        return userModels;
     }
 
     @PostMapping("users")
     public void createUser(@RequestBody User user) { service.save(user); }
 
     @GetMapping("users/{id}")
-    public EntityModel<User> getUserById(@PathVariable Integer id) {
+    public EntityModel<?> getUserById(@PathVariable Integer id) {
         try {
             User user = service.get(id);
-            return new EntityModel<>(
-                    user,
-                    linkTo(methodOn(UserController.class).getUserById(id)).withSelfRef(),
-                    linkTo(methodOn(UserController.class).list()).withRel("users")
-            );
+            return assembler.toModel(user);
         } catch (NoSuchElementException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return EntityModel.of(HttpStatus.NOT_FOUND);
         }
     }
 
