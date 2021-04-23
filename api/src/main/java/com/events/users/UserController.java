6c6b2a6e2d606c3455bd10b9ca71eaded5f674b2
VF-ROOT\pilkm001 <MatthewRichard.Pilkington@vcontractor.co.za>
2021-04-23 10:04:40 +0200

    Switching to personal PC because admin rights

--- api/src/main/java/com/events/users/UserController.java
+++ api/src/main/java/com/events/users/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -14,18 +17,28 @@ public class UserController {
     private UserService service;
 
     @GetMapping("users")
-    public List<User> list() {
-        return service.listAll();
+    public CollectionModel<EntityModel<User>> list() {
+
+        List<EntityModel<User>> users = service.listAll().stream()
+                .map(user -> getUserById(id))
+                .collect(Collectors.toList());
+
+        return new CollectionModel<>(users,
+                linkTo(methodOn(UserController.class).list()).withSelfRel());
     }
 
     @PostMapping("users")
     public void createUser(@RequestBody User user) { service.save(user); }
 
     @GetMapping("users/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Integer id) {
+    public EntityModel<User> getUserById(@PathVariable Integer id) {
         try {
             User user = service.get(id);
-            return new ResponseEntity<>(user, HttpStatus.OK);
+            return new EntityModel<>(
+                    user,
+                    linkTo(methodOn(UserController.class).getUserById(id)).withSelfRef(),
+                    linkTo(methodOn(UserController.class).list()).withRel("users")
+            );
         } catch (NoSuchElementException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
