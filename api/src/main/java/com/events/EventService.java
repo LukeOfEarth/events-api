41cadf19007576fb12f0ddd0ff2829300170df2e
Luke van Aardt <vanaardtluke@gmail.com>
2021-04-23 09:51:43 +0200

    added Optional to EventService error handling

--- api/src/main/java/com/events/EventService.java
+++ api/src/main/java/com/events/EventService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class EventService {
@@ -19,7 +21,11 @@ public class EventService {
     }
 
     public Event get(Integer id) {
-        return repo.findById(id).get();
+        Optional<Event> event = repo.findById(id);
+        if(event.isPresent()){
+            return event.get();
+        }
+        throw new NoSuchElementException("Unable to find event with id: "+id);
     }
 
     public  void delete(Integer id) {
