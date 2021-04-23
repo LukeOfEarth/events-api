e6f727786758aa2eb6869c9e8c778847815eb97b
Gert Dreyer <gert@bbd.co.za>
2021-04-23 10:11:22 +0200

    Takeon Add eventsusers linking table

---
+++ eventsusers.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `eventsdb`.`eventsusers` (
+	`Event_Id` INT NOT NULL,
+	`User_Id` INT NOT NULL,
+	`Status` INT NOT NULL,
+	FOREIGN KEY (`Event_Id`) REFERENCES event(Event_Id)	,
+	FOREIGN KEY (`User_Id`) REFERENCES user(User_Id),
+	PRIMARY KEY (Event_Id,User_Id)
+	);
