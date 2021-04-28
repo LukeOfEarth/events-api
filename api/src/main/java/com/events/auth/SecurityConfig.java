7675d58be0c5c89993533f3ad3a65269234d889a
Luke van Aardt <lukev@bbd.co.za>
2021-04-28 13:40:00 +0200

    auth fix

--- api/src/main/java/com/events/auth/SecurityConfig.java
+++ api/src/main/java/com/events/auth/SecurityConfig.java
@@ -1,17 +1,14 @@
 package com.events.auth;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
+        httpSecurity.csrf().disable()
                 .antMatcher("/**").authorizeRequests()
                 .antMatchers("/auth/login").permitAll()
                 .anyRequest().authenticated()
